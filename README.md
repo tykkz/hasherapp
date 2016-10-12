d138bc441e80fbbe4febd718b656dd9b58528b82
tykkz <user@tykkz>
2016-10-12 15:10:20 +0300

    Added Code Climate and Travis badges.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
+[![Build Status](https://travis-ci.org/tykkz/hasherapp.svg?branch=master)](https://travis-ci.org/tykkz/hasherapp)
+[![Code Climate](https://codeclimate.com/github/tykkz/hasherapp/badges/gpa.svg)](https://codeclimate.com/github/tykkz/hasherapp)
+[![Test Coverage](https://codeclimate.com/github/tykkz/hasherapp/badges/coverage.svg)](https://codeclimate.com/github/tykkz/hasherapp/coverage)
+[![Issue Count](https://codeclimate.com/github/tykkz/hasherapp/badges/issue_count.svg)](https://codeclimate.com/github/tykkz/hasherapp)
+
 # hasherapp
 An application that computes cryptographic hash value of a given text (and maybe file).
--- test_algorithm.py
+++ test_algorithm.py
@@ -4,3 +4,23 @@ from algorithm import hash_text
 class TestHashText(unittest.TestCase):
 	def setUp(self):
 		self.avail_func_array = ['md5', 'sha1', 'sha256']
+		self.avail_func_array2 = ['sha256', 'sha224']
+		self.text = "hello"
+		self.hello_pass_1 = {'sha1': 'aaf4c61ddcc5e8a2dabede0f3b482cd9aea9434d', 'sha256': '2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', 'md5': '5d41402abc4b2a76b9719d911017c592'}
+		self.hello_pass_100 = {'sha1': '3005a7cebc1edda34127c87c7bc79074e113015d', 'sha256': '0c7e20c4751943a4cf4ec6a2d17a520c8aaf1d2a714e2af1ad6667fd06c056de', 'md5': '3b8ca2de573942e58426494e9fcf8766'}
+
+	def test_successful(self):
+		self.assertEqual(self.hello_pass_1, hash_text(self.avail_func_array, self.text, 1)[1], "Correct hash values for one (1) pass of given hash algorithms")
+		self.assertItemsEqual(self.hello_pass_100, hash_text(self.avail_func_array, self.text, 100)[1], "Correct hash values for hundred (100) pass of given hash algorithms")
+
+	def test_empty_function_array(self):
+		self.assertFalse(hash_text([], self.text, 1)[0], "One should not use empty array for the hash algorithm list.")
+
+	def test_none_function_array_type(self):
+		self.assertFalse(hash_text(None, self.text, 1)[0], "Incorrect type for hash algorithms list.")
+
+	def test_diferent_function_array(self):
+		self.assertNotEqual(self.hello_pass_1, hash_text(self.avail_func_array2, self.text, 1), "Incorrect list of results, different hash algorithms used.")
+			
+if __name__ == '__main__':
+	unittest.main()
